0aa296c0a8efb9ba8fab64a4952c35a9fdf18e00
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-08-30 11:45:33 -0400

    updated misread one of the deliverables final submission

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const ramenDetailsRestaurant = document.querySelector('.restaurant')
 const ramenRatingDisplay = document.querySelector('#rating-display')
 const ramenRatingComment = document.querySelector('#comment-display')
 const newRamenForm = document.querySelector('#new-ramen')
-
-console.log(ramenDetailsRestaurant)
+const editRamenForm =document.querySelector('#edit-ramen')
 
 
 fetch("http://localhost:3000/ramens")
@@ -19,22 +18,44 @@ fetch("http://localhost:3000/ramens")
         ramenMenu.append(createRamenImage)
 
         createRamenImage.addEventListener('click', e =>{
-            ramenDetailsImage.src = ramen.image
             ramenDetailsName.textContent = ramen.name
             ramenDetailsRestaurant.textContent = ramen.restaurant
+            ramenDetailsImage.src = ramen.image
             ramenRatingComment.textContent = ramen.comment
             ramenRatingDisplay.textContent = ramen.rating
         })
+        ramenDetailsName.textContent = ramens[0].name
+        ramenDetailsRestaurant.textContent = ramens[0].restaurant
+        ramenDetailsImage.src = ramens[0].image
+        ramenRatingComment.textContent = ramens[0].comment
+        ramenRatingDisplay.textContent = ramens[0].rating
     })
 })
 
 newRamenForm.addEventListener('submit', e =>{
     e.preventDefault()
-    
+    createRamenImage = document.createElement('img')
+    createRamenImage.setAttribute('src', e.target[2].value)
+    ramenMenu.append(createRamenImage)
+
+    const newRamenName = e.target[0].value
+    const newRamenRestaurant = e.target[1].value
+    const newRamenImage = e.target[2].value
+    const newRamenDisplay = e.target[3].value
+    const newRamenRating = e.target[4].value
+
     ramenDetailsName.textContent = e.target[0].value
     ramenDetailsRestaurant.textContent = e.target[1].value
     ramenDetailsImage.src  = e.target[2].value
     ramenRatingDisplay.textContent = e.target[3].value
     ramenRatingComment.textContent = e.target[4].value
-    
+
+    createRamenImage.addEventListener('click', e =>{
+        ramenDetailsName.textContent = newRamenName
+            ramenDetailsRestaurant.textContent = newRamenRestaurant
+            ramenDetailsImage.src = newRamenImage
+            ramenRatingDisplay.textContent = newRamenDisplay
+            ramenRatingComment.textContent = newRamenRating
+            
+    })
 })
